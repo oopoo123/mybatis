ba2ce8ffc854ccf2c8d011e98dd84aba2868c5e6
oopoo123 <oopoo123@naver.com>
2024-01-11 13:31:55 +0900

    게시판 글, 수정, 검색 기능 추가

--- src/main/java/com/jbedu/mybatis/controller/MybatisController.java
+++ src/main/java/com/jbedu/mybatis/controller/MybatisController.java
@@ -2,6 +2,8 @@ package com.jbedu.mybatis.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jbedu.mybatis.dao.MybatisDao;
 import com.jbedu.mybatis.dto.MybatisDto;
-
 
 @Controller
 public class MybatisController {
@@ -22,13 +23,72 @@ public class MybatisController {
 	public String list(Model model) {
 		
 		//MybatisDao dao = new MybatisDao();
-		MybatisDao dao = sqlSession.getMapper(MybatisDao.class); // dao 객체 분리 생성
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);//dao 객체 분리 생성
 		
 		ArrayList<MybatisDto> dtos = dao.listDao();
 		
-		model.addAttribute("fboardDtos", dtos);		
+		model.addAttribute("fboardDtos", dtos);
 		
 		return "list";
+	}
+	
+	@RequestMapping(value = "/write_form")
+	public String write_form() {
+		return "write_form";
+	}
+	
+	@RequestMapping(value = "/write")
+	public String write(HttpServletRequest request) {
+		
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);//dao 객체 분리 생성
+		dao.writeDao(request.getParameter("fbname"), request.getParameter("fbtitle"), request.getParameter("fbcontent"));
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping(value = "/content_view")
+	public String content_view(HttpServletRequest request, Model model) {
+		
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);//dao 객체 분리 생성
+		model.addAttribute("fboardDto", dao.contentDao(request.getParameter("fbnum")));
+		
+		return "content_view";
+	}
+	
+	@RequestMapping(value = "/modify_form" )
+	public String modify_form(HttpServletRequest request, Model model) {
+		
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);//dao 객체 분리 생성
+		model.addAttribute("fboardDto", dao.contentDao(request.getParameter("fbnum")));
+		
+		return "modify_form";
+	}
+	
+	@RequestMapping(value = "/modify")
+	public String modify(HttpServletRequest request, Model model) {
+		
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);
+		dao.modifyDao(request.getParameter("fbtitle"), request.getParameter("fbname"), request.getParameter("fbcontent"), request.getParameter("fbnum"));
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping(value = "/search")
+	public String search(HttpServletRequest request, Model model) {
+		
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);//dao 객체 분리 생성
+		model.addAttribute("fboardDtos", dao.searchDao(request.getParameter("searchKey")));
+		
+		return "list";
+	}
+	
+	@RequestMapping(value = "/delete")
+	public String delete(HttpServletRequest request, Model model) {
+		
+		MybatisDao dao = sqlSession.getMapper(MybatisDao.class);
+		dao.deleteDao(request.getParameter("fbnum"));
+		
+		return "redirect:list";
 	}
 
 }
--- src/main/java/com/jbedu/mybatis/dao/MybatisDao.java
+++ src/main/java/com/jbedu/mybatis/dao/MybatisDao.java
@@ -7,5 +7,10 @@ import com.jbedu.mybatis.dto.MybatisDto;
 public interface MybatisDao { // 인터페이스는 추상메서드만 쓰는데 추상메서드는 이름만 있는 것
 	
 	public ArrayList<MybatisDto> listDao(); //게시판 모든 글 목록 가져오기
+	public void writeDao(String fbname, String fbtitle, String fbcontent); //게시판 글쓰기 메서드
+	public MybatisDto contentDao(String fbnum); //게시판 글 내용 가져오기
+	public void modifyDao(String fbtitle, String fbname, String fbcontent, String fbnum); //게시판 글 수정하기
+	public ArrayList<MybatisDto> searchDao(String searchKey); // 제목 또는 내용에 특정단어 포함 결과 검색
+	public void deleteDao(String fbnum); //게시판 글 삭제
 
 }
